27768e81903a1c8e73607c5e9227f0b4dcc4826e
Phil McAdams <pdm1999@gmail.com>
2015-08-01 13:55:40 -0600

    Initial PhilTest project creation

---
+++ PhilTest/PhilTest.h
@@ -0,0 +1,32 @@
+
+// PhilTest.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CPhilTestApp:
+// See PhilTest.cpp for the implementation of this class
+//
+
+class CPhilTestApp : public CWinApp
+{
+public:
+	CPhilTestApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CPhilTestApp theApp;
